3be487e47c71a0c03ee62e02418c7dcc2e64fa37
Soma Limbek <soma.limbek@gmail.com>
2022-03-23 16:20:32 +0100

    Create Xcode project

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Soma Limbek on 2022. 03. 23..
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
